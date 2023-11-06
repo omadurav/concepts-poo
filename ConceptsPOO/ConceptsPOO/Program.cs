bbdce9b2eecdb76d5875a877e29f8023cf7ceecf
omadurav <omadurav@outlook.com>
2023-11-06 09:32:09 -0500

    Finish date class.

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConceptsPOO;
+
+Console.WriteLine("POO Fundamentals");
+Console.WriteLine("================");
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
