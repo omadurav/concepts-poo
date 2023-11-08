8a0b94d0185a7361c0c034cc4942736d3f23a7d1
omadurav <omadurav@outlook.com>
2023-11-07 21:31:01 -0500

    Employee, SalaryEmployee and CommisionEmploye

---
+++ ConceptsPOO/ConceptsPOO/ComissionEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class ComissionEmployee : Employee
+    {
+        public float ComissionPercentaje { get; set; }
+        public decimal Sales { get; set; }
+        public override decimal GetValueToPay()
+        {
+            return Sales * (decimal)ComissionPercentaje;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tCommision percentaje: {$"{ComissionPercentaje:P2}",18}" +
+                $"\n\tSales...............: {$"{Sales:C2}",18}" +
+                $"\n\tValue to pay........: {$"{GetValueToPay}",18}";
+        }
+    }
+}
